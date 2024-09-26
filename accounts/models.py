04e640c8fd4652460ce005c18e9fb681e2a2f7c8
kim-sangah <kimsangah_@naver.com>
2024-09-26 16:31:13 +0900

    update: Update accounts models.py
    유저 모델 업데이트

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.contrib.auth.models import (
+    BaseUserManager, AbstractBaseUser
+)
 
-# Create your models here.
+
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None):
+
+        if not email:
+            raise ValueError('이메일을 입력해주세요')
+
+        user = self.model(
+            email=self.normalize_email(email),
+        )
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser):
+    gender_choice = (("M","남성"),("F","여성"))
+
+    email = models.EmailField(max_length=20, unique=True,)
+    password = models.CharField(max_length=20)
+    username = models.CharField(max_length=20)
+    gender = models.CharField(max_length=1, choices=gender_choice, null=True, blank=True)
+    birthday = models.DateField(auto_now_add=True, blank=True)
+    is_active = models.BooleanField(default=True)
+    is_admin = models.BooleanField(default=False)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'

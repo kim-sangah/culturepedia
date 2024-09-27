a90cdec45b68f5bf663645cb113327cea0c14c84
lguewk06 <lguewk06@gmail.com>
2024-09-27 09:21:26 +0900

    fix: accounts models.py merge accounts model 수정

--- accounts/models.py
+++ accounts/models.py
@@ -3,12 +3,13 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    gender_choice = (("M","남성"),("F","여성"))
+    gender_choice = (("M", "남성"), ("F", "여성"))
 
     email = models.EmailField(max_length=20, unique=True)
     password = models.CharField(max_length=20)
     username = models.CharField(max_length=20)
-    gender = models.CharField(max_length=1, choices=gender_choice, null=True, blank=True)
+    gender = models.CharField(
+        max_length=1, choices=gender_choice, null=True, blank=True)
     birthday = models.DateField(auto_now_add=True, blank=True)
 
     USERNAME_FIELD = 'email'

8343c49ddc01949e07d0b2d17e02bb15b30c9948
kim-sangah <kimsangah_@naver.com>
2024-10-01 20:53:53 +0900

    update: Add review views.py
    리뷰 뷰 작성

--- performances/models.py
+++ performances/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from accounts.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=20)
+
+
+class Review(models.Model):
+    article_id = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    title = models.CharField(max_length=20)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

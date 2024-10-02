3783fac4057d733536008bb19069a3c371bf0eef
kim-sangah <kimsangah_@naver.com>
2024-10-02 22:51:30 +0900

    update: Add kopis API DB
    공연예술 API DB 저장 추가

---
+++ kopis_api.py
@@ -0,0 +1,37 @@
+import requests
+import json
+from culturepedia import config
+import xmltodict
+
+api_key = config.API_KEY
+
+performance_res = []
+
+for pageNum in range(1, 37):
+    url = f'http://www.kopis.or.kr/openApi/restful/pblprfr?service={api_key}&stdate=20240901&eddate=20241030&rows=100&cpage={pageNum}'
+    response = requests.get(url)
+    data = xmltodict.parse(response.content)
+
+    for item in data['dbs']['db']:
+        try:
+            dict = {
+                    "model" : "performances.performance",
+                    "pk" : item['mt20id'],
+                    'fields':
+                    {
+                        "title": item['prfnm'],
+                        "start_date": item['prfpdfrom'],
+                        "end_date": item['prfpdto'],
+                        "facility_name": item['fcltynm'],
+                        "type": item['genrenm'],
+                        "state": item['prfstate']
+                    }
+                    }
+            performance_res.append(dict)
+        except:
+            pass
+
+
+with open('performance_370.json', "w", encoding='utf-8') as f:
+
+    json.dump(performance_res, f, ensure_ascii=False, indent=4)
--- performances/models.py
+++ performances/models.py
@@ -14,30 +14,15 @@ class Facility(models.Model):
 
 
 class Performance(models.Model):
-    facility_id = models.ForeignKey('Facility', on_delete=models.CASCADE, null=True) #외래키 참조
+    kopis_id = models.CharField(primary_key=True, max_length=10)
+    facility_name = models.CharField(max_length=100)  #외래키 참조
     title = models.CharField(max_length=100, null=True)
     
     #공연시작날짜만 들어감
-    start_date = models.DateField() 
-    end_date = models.DateField()
-    cast = models.CharField(max_length=10)
-    crew = models.CharField(max_length=10)
-    runtime = models.IntegerField(null= True)
-    age = models.IntegerField()
-    entprsmnP = models.CharField(max_length=100)
-    entrprsnmA = models.CharField(max_length=100)
-    pricing = models.IntegerField()# 가격
-    
-    poster = models.ImageField(upload_to="") #일단 경로는 빈칸
-    synopsis = models.CharField(max_length=255)
+    start_date = models.CharField(max_length=20) 
+    end_date = models.CharField(max_length=20)
     type = models.CharField(max_length=10)
     state = models.CharField(max_length=10)
-    visit = models.BooleanField(default=False)
-    daehakro = models.BooleanField(default=False)
-    festival = models.BooleanField(default=False)
-    musicallicense = models.BooleanField(default=False)
-    musicalcreate = models.BooleanField(default=False)
-    dtguidance = models.CharField(max_length=100)
 
 
 class Article(models.Model):

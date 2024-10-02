5bc7a8f97c0691b15c7c6a181cbb7bf56914c5e9
lguewk06 <lguewk06@gmail.com>
2024-10-02 15:53:05 +0900

    merge: lookup + detail_lookup 금주 랭킹, 상세조회(공연, 공연장), 검색(공연명, 공연장명, 제작사, 출연진) 기능

--- performances/urls.py
+++ performances/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import OPENAPIViews
+from .views import OPENAPIViews, OPENAPISearchViews, OPENAPIDetailViews
 
 
 urlpatterns = [
     path('kopis/', OPENAPIViews.as_view()),
+    path('kopis/search/<str:pk>/', OPENAPISearchViews.as_view()),
+    path('kopis/<str:pk>/', OPENAPIDetailViews.as_view()),
 ]

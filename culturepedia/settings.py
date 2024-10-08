14c3a43b3c641ba5e7b15e81d7d82f58c8af5e2e
HYUN1103 <jin_113@naver.com>
2024-10-08 23:23:58 +0900

    add : web

--- culturepedia/settings.py
+++ culturepedia/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     # app
     'accounts',
     'performances',
+    'web',
 ]
 
 MIDDLEWARE = [
--- culturepedia/urls.py
+++ culturepedia/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/accounts/', include('accounts.urls')),
     path('api/performances/', include('performances.urls')),
+    path('web/', include('web.urls')),
+    
 ]

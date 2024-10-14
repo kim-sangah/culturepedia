e43d73c80e4737277a4fe0ec9658d3df9aa5c8f5
kim-sangah <kimsangah_@naver.com>
2024-10-14 11:37:20 +0900

    fix: Update DB postgreSQL
    데이터베이스 프로그램 변경

--- culturepedia/settings.py
+++ culturepedia/settings.py
@@ -107,11 +107,14 @@ WSGI_APPLICATION = 'culturepedia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE' : config.DB_ENGINE,
+        'NAME' : config.DB_NAME,
+        'USER' : config.DB_USER,
+        'PASSWORD' : config.DB_PASSWORD,
+        'HOST' : config.DB_HOST,
+        'PORT' : config.DB_PORT,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
--- performances/tasks.py
+++ performances/tasks.py
@@ -70,8 +70,7 @@ def start_scheduler():
     # scheduler 실행 중이 아닌 경우
     if not hasattr(start_scheduler, 'scheduler_running'):
         scheduler = BackgroundScheduler(timezone=settings.TIME_ZONE)
-        # scheduler.add_jobstore(jobstores.DjangoJobStore(),
-        #                     "default")  # Django DB에 저장
+        # scheduler.add_jobstore(jobstores.DjangoJobStore(), "default")  # Django DB에 저장
 
     # 매일 자정 procees_scripts 실행
     scheduler.add_job(

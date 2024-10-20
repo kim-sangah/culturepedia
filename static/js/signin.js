35e96fb17ab0b8338614c2be9f67a1f07aa8413f
lguewk06 <lguewk06@gmail.com>
2024-10-20 09:53:13 +0900

    bugfix: 로그인시 화면 이동

--- static/js/signin.js
+++ static/js/signin.js
@@ -1,76 +1,31 @@
-// JWT 토큰을 로컬 스토리지에서 가져오는 함수
-function getJwtToken() {
-    return localStorage.getItem('access_token');
+function isLoggedIn() {
+    return document.cookie.includes('user_id=');
 }
 
-// 서버에서 유저 아이디 받아오기
-function fetchCurrentUserId() {
-    const token = getJwtToken();
-
-    return fetch('/api/user/status/', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`
-        }
-    })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Failed to fetch user info');
-            }
-            return response.json();
-        })
-        .then(data => {
-            return data.user_id;
-        })
-        .catch(error => {
-            console.error('Error fetching user ID:', error);
-            return null;
-        });
-}
-
-// 사용자 인증 상태 확인 함수, 인증 상태에 따라 UI 업데이트
-function checkUserAuthentication() {
-    const token = getJwtToken();
+// 로그인 상태에 따라 표시되는 버튼 바꾸기
+window.onload = function () {
     const signinBtn = document.getElementById('signin-btn');
     const signupBtn = document.getElementById('signup-btn');
     const signoutBtn = document.getElementById('signout-btn');
     const profileBtn = document.getElementById('profile-btn');
     const recommendationsBtn = document.getElementById('nav-recommendations-btn');
 
-    if (!token) {
-        // 토큰이 없는 경우
+    if (isLoggedIn()) {
+        // 로그인 되어있으면 signout, profile 버튼만 보이게
+        signinBtn.style.display = 'none';
+        signupBtn.style.display = 'none';
+        signoutBtn.style.display = 'block';
+        profileBtn.style.display = 'block';
+        recommendationsBtn.style.display = 'block';
+    } else {
+        // 로그인 되어있지 않으면 signin, signup 버튼만 보이게
         signinBtn.style.display = 'block';
         signupBtn.style.display = 'block';
         signoutBtn.style.display = 'none';
-        profileBtn.display = 'none';
+        profileBtn.style.display = 'none';
         recommendationsBtn.style.display = 'none';
-        return;
     }
-
-    // JWT 토큰을 Authorization 헤더에 추가하여 API 요청
-    fetch('/api/user/status/', {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${token}`,
-        }
-    })
-        .then(response => {
-            if (response.ok) {
-                signinBtn.style.display = 'none';
-                signupBtn.style.display = 'none';
-                signoutBtn.style.display = 'block';
-                profileBtn.display = 'block';
-                recommendationsBtn.style.display = 'block';
-            } else {
-                signinBtn.style.display = 'block';
-                signupBtn.style.display = 'block';
-                signoutBtn.style.display = 'none';
-                profileBtn.display = 'none';
-                recommendationsBtn.style.display = 'none';
-            }
-        })
-        .catch(error => console.error('Error fetching user status:', error));
-}
+};
 
 document.addEventListener("DOMContentLoaded", () => {
     const signinBtn = document.getElementById('signin-btn');
@@ -103,8 +58,10 @@ document.addEventListener("DOMContentLoaded", () => {
             .then(data => {
                 console.log(data);
                 // 로그인 성공 시 쿠키에 유저 아이디 저장
+                var result = document.getElementById('result');
+                result.innerHTML = '';
                 document.cookie = `user_id=${data.user_id}; path=/`;
-                window.history.back();       // 이전 페이지로 돌아가기
+                result.innerHTML = `<div>${data.refresh}</div>`
             })
             .catch(error => {
                 console.error('Error:', error);
